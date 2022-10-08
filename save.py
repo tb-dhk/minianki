633bceac664333c5cf8533c9dab59152e37f7fec
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-10-08 14:40:43 +0800

    Update and rename exports.py to save.py

---
+++ save.py
@@ -0,0 +1,10 @@
+from main import deck
+import csv
+
+writer1 = txt.writer(open('.txt.mnak', 'w'))
+writer2 = csv.writer(open('.csv.mnak', 'w'))
+
+# save to 
+for x in deck:
+  writer1.writerow(x.term + "  " + x.defin)
+  writer2.writerow([x.term, x.defin, x.ls, x.ease, x.lastint, x.duedate])
