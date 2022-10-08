0f1963997032aad56fdf9cb8469e4facd3f48537
shua <86475694+shuu-wasseo@users.noreply.github.com>
2022-10-08 14:02:08 +0800

    Update exports.py

--- exports.py
+++ exports.py
@@ -1,24 +1,10 @@
-from init import deck
+from learn import deck
 import csv
 
-fileformat = input("would you like to export in .csv or .txt format?")
+fileformat = input("would you like to export with scheduling? Y/n")
 match fileformat:
-  case ".csv":
-    # if exporting to csv
-    reader = csv.reader(open('.csv.mnak', 'w'))
-    writer = csv.writer(open('.csv.mnak', 'w'))
-    
-    # check if exports file has any data
-    try:
-      reader[0]
-    except:
-      writer.writerow(["term", "defin", "ls", "ease", "lastint", "duedate"])
-    finally:
-      for x in deck:
-        writer.writerow([x.term, x.defin, x.ls, x.ease, x.lastint, x.duedate])
-
   case ".txt":
-    # if exporting to txt
+    # if without scheduling
     reader = csv.reader(open('.txt.mnak', 'w'))
     writer = csv.writer(open('.txt.mnak', 'w'))
     
@@ -30,3 +16,17 @@ match fileformat:
     finally:
       for x in deck:
         writer.writerow([x.term, x.defin])
+
+  case _:
+    # if with scheduling
+    reader = csv.reader(open('.csv.mnak', 'w'))
+    writer = csv.writer(open('.csv.mnak', 'w'))
+    
+    # check if exports file has any data
+    try:
+      reader[0]
+    except:
+      writer.writerow(["term", "defin", "ls", "ease", "lastint", "duedate"])
+    finally:
+      for x in deck:
+        writer.writerow([x.term, x.defin, x.ls, x.ease, x.lastint, x.duedate])
