5c553e273c82b73e25341dcc1f61cd24e1c0141e
shuu-wasseo <joshuasoh2008@gmail.com>
2022-10-13 23:50:58 +0800

    fixed update

--- minianki.py
+++ minianki.py
@@ -7,6 +7,7 @@ import subprocess
 
 os.chdir(os.path.dirname(os.path.realpath(__file__)))
 
+print("setting up...")
 subprocess.run(["git", "init"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 subprocess.run(["git", "branch", "-m", "main"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
 subprocess.run(["git", "remote", "add", "minianki", "https://github.com/shuu-wasseo/minianki"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
@@ -513,6 +514,10 @@ def deck(deck):
                             case _:
                                 print("    invalid. try again")
 def update():
+    print("recloning minianki...")
     subprocess.run(["git", "-C", os.path.abspath(os.path.join(os.getcwd(), os.pardir)), "clone", "https://github.com/shuu-wasseo/minianki"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("cleaning up the mess...")
     subprocess.run(["git", "clean", "-f", "-d"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("repulling minianki...")
     subprocess.run(["git", "pull", "https://github.com/shuu-wasseo/minianki"], stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    print("done!")
